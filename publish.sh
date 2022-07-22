475e3cc8109918e3b2a35af14acc8b891f7c4691
wira.indra <wira.indra@bukalapak.com>
2022-07-22 12:59:44 +0700

    add template

--- publish.sh
+++ publish.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-jupyter-nbconvert --to html notebooks/index.ipynb --reveal-prefix=reveal.js
+jupyter-nbconvert --to html --template lab --theme dark notebooks/index.ipynb
 mv notebooks/index.html  index.html
 
-cd notebooks
-arr=(*.ipynb)
-cd ..
-for f in "${arr[@]}"; do
-   # Chop off the extension
-   filename=$(basename "$f")
-   extension="${filename##*.}"
-   filename="${filename%.*}"
+# cd notebooks
+# arr=(*.ipynb)
+# cd ..
+# for f in "${arr[@]}"; do
+#    # Chop off the extension
+#    filename=$(basename "$f")
+#    extension="${filename##*.}"
+#    filename="${filename%.*}"
 
-   # Convert the Notebook to HTML
-   jupyter-nbconvert --to html notebooks/"$filename".ipynb --reveal-prefix=reveal.js
-   # Move to the Html directory
-   mv notebooks/"$filename".html  pages/"$filename".html
+#    # Convert the Notebook to HTML
+#    jupyter-nbconvert --to html notebooks/"$filename".ipynb --reveal-prefix=reveal.js
+#    # Move to the Html directory
+#    mv notebooks/"$filename".html  pages/"$filename".html
 
 #    # Convert the Notebook to slides
 #    jupyter-nbconvert --to slides notebooks/"$filename".ipynb --reveal-prefix=reveal.js
