36f07178e51ada8fa8a60aea5645d6c7c4130133
wira.indra <wira.indra@bukalapak.com>
2022-07-22 14:52:35 +0700

    fix publish

--- publish.sh
+++ publish.sh
@@ -3,33 +3,13 @@
 jupyter-nbconvert --to html --template classic notebooks/index.ipynb
 mv notebooks/index.html  index.html
 
-# cd notebooks
-# arr=(*.ipynb)
-# cd ..
-# for f in "${arr[@]}"; do
-#    # Chop off the extension
-#    filename=$(basename "$f")
-#    extension="${filename##*.}"
-#    filename="${filename%.*}"
-
-#    # Convert the Notebook to HTML
-#    jupyter-nbconvert --to html notebooks/"$filename".ipynb --reveal-prefix=reveal.js
-#    # Move to the Html directory
-#    mv notebooks/"$filename".html  pages/"$filename".html
-
-# done
-arr=$(find . -iname "*.ipynb" -exec basename {} .ipynb ';')
+arr=$(find notebooks -iname "*.ipynb" -exec basename {} .ipynb ';')
 for nb in $arr; do
-    # extension="${nb##*.}"
-    # nb=$(basename "$nb")
-    # filename="${nb%.*}"
     # Convert the Notebook to HTML
-    jupyter-nbconvert --to html notebooks/"$nb".ipynb
+    jupyter-nbconvert --to html --template classic notebooks/"$nb".ipynb
     # Move to the Html directory
-    mv notebooks/"$nb".html  pages/"$nb".html
+    mv notebooks/"$nb".html pages/"$nb".html
 done
-
-
 
 # Push the updates to gh-pages
 mkdir -p /tmp/workspace
