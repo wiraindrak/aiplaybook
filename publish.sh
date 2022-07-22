ad09fec2e24ae64d7c88c48f89d39f58773edf45
wira.indra <wira.indra@bukalapak.com>
2022-07-22 14:50:05 +0700

    update pages

--- publish.sh
+++ publish.sh
@@ -18,6 +18,18 @@ mv notebooks/index.html  index.html
 #    mv notebooks/"$filename".html  pages/"$filename".html
 
 # done
+arr=$(find . -iname "*.ipynb" -exec basename {} .ipynb ';')
+for nb in $arr; do
+    # extension="${nb##*.}"
+    # nb=$(basename "$nb")
+    # filename="${nb%.*}"
+    # Convert the Notebook to HTML
+    jupyter-nbconvert --to html notebooks/"$nb".ipynb
+    # Move to the Html directory
+    mv notebooks/"$nb".html  pages/"$nb".html
+done
+
+
 
 # Push the updates to gh-pages
 mkdir -p /tmp/workspace
